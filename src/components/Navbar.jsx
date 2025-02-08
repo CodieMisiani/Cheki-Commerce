93fac2ed71eb143908f1279a4b83c701abc33a3b
Phyne <phynevni@gmail.com>
2025-02-08 21:08:48 +0300

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/Gina.png'
 import './Navbar.css'
+import { TiShoppingCart } from "react-icons/ti";
 
 const Navbar = () => {
   return (
@@ -23,6 +24,26 @@ const Navbar = () => {
                 <li>
                     <Link to="/contact">Contact</Link>
                 </li>
+            </ul>
+
+            <ul className='nav-links sign-cart'>
+                {/* <li>
+                    <Link to="/">Home</Link>
+                </li> */}
+                <li className='sign'>
+                    <Link to="/signup" className='signing'>Signup</Link>
+                    <span>OR</span>
+                    <Link to="/signin" className='signing'>Signin</Link>
+                </li>
+                <li className='cart'>
+                    <Link to="/cart" className='cart-icon'>
+                        <span className="cart-icon-size">
+                        <TiShoppingCart />
+                        <span className='count'>0</span>
+                        </span>
+                    </Link>
+                </li>
+  
             </ul>
         </div>
       </nav>
